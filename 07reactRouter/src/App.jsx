26507c65a56453191a360129daa6729f3581fd1d
Roshan <roshangupta170@gmail.com>
2024-03-16 16:52:17 +0530

    routing hogaya

---
+++ 07reactRouter/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Header from './components/Header/Header'
+
+function App() {
+  return (
+    <>
+      <h1 className='bg-green-600 p-4'>Hello Routers</h1>
+    </>
+  )
+}
+
+export default App
